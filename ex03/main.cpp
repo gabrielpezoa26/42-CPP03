6b9585f6e2b196158ab1bc3843c54281740a94f4
Gabriel Pezoa <gabrielpezoa26@gmail.com>
2025-11-13 23:33:25 -0300

    ex03: added tests to main

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 17:23:39 by gcesar-n          #+#    #+#             */
-/*   Updated: 2025/11/13 20:02:05 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2025/11/13 22:04:56 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,32 @@
 
 int main()
 {
-	DiamondTrap a("eddie");
+	logColored("-------Displaying constructors output-------", GREEN);
+	ClapTrap a("Eddie");
+	DiamondTrap b("Murphy");
+	log("\n");
 
-	log("--------------");
-	a.whoAmI();
+
+	logColored("-------Testing ClapTrap methods again-------", YELLOW);
+	a.attack("Zombie");
+	a.takeDamage(5);
+	a.beRepaired(4);
+	a.printStats();
+	log("\n");
+
+
+	logColored("-------Now testing DiamondTrap own methods-------", BLUE);
+	b.attack("Zombie");
+	b.whoAmI();
+	log("\n");
+
+
+	logColored("-------Now testing DiamondTrap inherited methods-------", BLUE);
+	b.takeDamage(3);
+	b.beRepaired(2);
+	b.printStats();
+	log("\n");
+
+	logColored("-------Displaying destructors output-------", RED);
+	return 0;
 }
